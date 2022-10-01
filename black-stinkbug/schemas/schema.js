8fd140e607e14b62aa26e9cdc6eb344cbd416d3e
Severus KEY <severus.key308@gmail.com>
2022-10-01 17:40:12 +0200

    5th commit

--- black-stinkbug/schemas/schema.js
+++ black-stinkbug/schemas/schema.js
@@ -4,8 +4,23 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
+// We import object and document schema
+import property from './property'
+import propertyImage from './propertyImage'
+import review from './Review'
+import traveller from './Traveller'
+import host from './Host'
+import person from './Person'
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([
+    property,
+    propertyImage,
+    review,
+    traveller,
+    host,
+    person,
+  ]),
 })
